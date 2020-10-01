a974c6acf69b90d395d40ea0a58bee3e74612a41
maxim <satamaks@gmail.com>
2020-10-01 18:36:56 +0300

    update template

--- Shop/shopApp/views.py
+++ Shop/shopApp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from shopApp import models
+
 def index(request):
+    latest_news =models.New
     return render(request, "index.html")
 # Create your views here.
